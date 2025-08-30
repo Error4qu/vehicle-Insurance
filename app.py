3cc5dedfa5326f210595bb40935e85dfcad8591e
Error4qu <madhav4412@gmail.com>
2025-08-30 09:22:31 +0530

    Data_INGESION DONE

---
+++ app.py
@@ -0,0 +1,5 @@
+# y54ePXbhV8Y9dkBg
+# madhav4412_db_user
+# mongodb+srv://madhav4412_db_user:<db_password>@cluster0.4rzp1bq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+# $env:MONGODB_URL ="mongodb+srv://madhav4412_db_user:<db_password>@cluster0.4rzp1bq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
